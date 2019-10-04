b967cb631c6a028a2669139cbf15e856b4fcf449
Lesha <Lesha@sobaka.gav>
2019-10-04 19:12:16 +0300

    punkt4

--- client.py
+++ client.py
@@ -1,24 +1,36 @@
 import socket
 
 sock = socket.socket()
+print("***Введите IP или L для поключения к локальному серверу***")
+Ip=input()
+if Ip=="L":
+	Ip='localhost'
+else:
+	pass
+
+print("***Введите порт или L для поключения к локальному серверу***")
+port=input()
+if port=="L":
+	port=9090
+else:
+	pass
+print("ip=",Ip, "port=",port)
 try:
-	sock.connect(('localhost', 9090))
+	sock.connect((Ip, port))
 	print("Подключение прошло успешно, введите сообщение")
 	msg=""
-	msg=input()
 	print("отправка данных")
 	try:
-		msg=""
 		msg=input()
 		while msg!="stop":
 			sock.send(msg.encode())
 			data = sock.recv(1024)
 			print("данные отправлены")			
 			msg=input()
-			pass
 		 
 	except:
 		print("данные не отправлены")
+		sock.close
 
 	sock.close()
 	data = sock.recv(1024)
--- server.py
+++ server.py
@@ -1,33 +1,55 @@
 import socket
+import datetime
+
+
+file = 'logs.txt'
+logs=[]
+
+
 
 sock = socket.socket()
+
 sock.bind(('', 9090))
-print("сервер запущен")
+print("***Сервер запущен***")
+logs.append(str(datetime.datetime.now())+" запущен сервер")
+
 sock.listen(1)
+
+
 
 command=''
 while command!="stop":
 	
 	sock.listen(1)
-	print("ожидание подключ")
+	print("***Ожидание подключения***")
 	conn, addr = sock.accept()
-
-	print ('соединение с:', addr)
-
-
-	
-	counter=0
+	logs.append(str(datetime.datetime.now())+"Установлено соединение с "+addr[0])
+	print ('***Установлено соединение с:', addr,'***')
 
 	while True:
-	    msg=''
 	    data = conn.recv(1024)
+	    msg=''
 	    if not data:
 	        break
-	    msg+=data.decode()
+	    msg+=data.decode() 
 	    conn.send(data.upper())
 	    print(msg)
-	print("продолжить или отключить сервер?")
+	logs.append(str(datetime.datetime.now())+" пользователь "+ addr[0]+" отключился")
+	print("***Пользователь "+addr[0]+" отключился***")
+	print("***Продолжить или отключить сервер?***")
 	command=input()
+logs.append(str(datetime.datetime.now())+" сервер завершает работу")
+logs.append("---"*20)
+
+try:
+	f=open(file,'a')
+	for i in logs:
+		f.write(i+'\n')
+except:
+	f=open(file,'w')
+	for i in logs:
+		f.write(i+'\n')
+f.close()
 conn.close()
 
 """
